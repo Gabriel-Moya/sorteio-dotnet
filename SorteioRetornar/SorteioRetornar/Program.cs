c56a0302110eb1a81dfea4cc35e6ec878cd26dbf
Gabriel Moya <gabrielmoya123@gmail.com>
2022-12-21 18:58:43 -0300

    Utilizando a connection string

--- SorteioRetornar/SorteioRetornar/Program.cs
+++ SorteioRetornar/SorteioRetornar/Program.cs
@@ -1,25 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using SorteioRetornar.Infra.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+ConfigureServices(builder);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
+
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+}
