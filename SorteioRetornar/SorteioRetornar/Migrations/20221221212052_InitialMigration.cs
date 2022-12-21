3c3e56ca6b229e83be8bcddc7e70876ae63dab1b
Gabriel Moya <gabrielmoya123@gmail.com>
2022-12-21 18:58:52 -0300

    Adicionando migration inicial

---
+++ SorteioRetornar/SorteioRetornar/Migrations/20221221212052_InitialMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SorteioRetornar.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "VARCHAR(11)", maxLength: 11, nullable: false),
+                    Cpf = table.Column<string>(type: "VARCHAR(11)", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: false),
+                    GeneratedNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_Email",
+                table: "Client",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_GeneratedNumber",
+                table: "Client",
+                column: "GeneratedNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
